14a6853b80692b0eec7abbff917fd1bfe4378a27
lebao3105 <bao12345yocoo@gmail.com>
2023-06-29 16:56:09 +0700

    Include StyledTextControl

--- tests/test_tk.py
+++ tests/test_tk.py
@@ -4,6 +4,7 @@ import tkinter.ttk as ttk
 from libtextworker import _importer
 _importer.test_import("tkinter")
 from libtextworker.interface.tk import ColorManager
+from libtextworker.interface.tk.editor import StyledTextControl
 
 clrmgr = ColorManager()
 
@@ -16,14 +17,22 @@ def test_tk():
     ttk.Label(fm, text="Hello world!").pack()
     ttk.Button(fm, text="This is a button").pack()
     ttk.Checkbutton(fm, text="A checkbutton").pack()
+
     nb = ttk.Notebook(fm)
+    
     pb = ttk.Progressbar(nb, phase=8, value=0.0)
-    nb.add(ttk.Combobox(nb, values=["one", "two", "three"]), text="Tab 1")
-    nb.add(pb, text="Tab 2")
-    nb.pack(expand=True, fill="both")
     pb.start()
 
+    te = StyledTextControl(nb)
+    te.EditorInit()
+
+    nb.add(ttk.Combobox(nb, values=["one", "two", "three"]), text="Tab 1")
+    nb.add(pb, text="Tab 2")
+    nb.add(te, text="Tab 3")
+
     clrmgr.configure(fm, True)
-    fm.pack()
+    clrmgr.configure(nb, True)
+    nb.pack(expand=True, fill="both")
+    fm.pack(expand=True, fill="both")
 
     app.mainloop()
