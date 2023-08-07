cccc3d7565b1c719a9481c56d148accb7a81a41d
lebao3105 <bao12345yocoo@gmail.com>
2023-08-07 23:37:56 +0700

    wxDirList & move wxImageList outside wxDirCtrl

--- libtextworker/interface/wx/dirctrl.py
+++ libtextworker/interface/wx/dirctrl.py
@@ -1,11 +1,26 @@
 import os
 import wx
 
-# from jaraco.path import
 from libtextworker.general import CraftItems
 
 
-# Referenced from https://python-forum.io/thread-8513.html
+imgs = wx.ImageList(16, 16)
+def addImg(name: str) -> int:
+    return imgs.Add(
+        wx.ArtProvider.GetBitmap(
+            getattr(wx, f"ART_{name.upper()}"),
+            wx.ART_OTHER,
+            (16, 16)
+        )
+    )
+
+folderidx = addImg("folder")
+fileidx = addImg("normal_file")
+openfolderidx = addImg("folder_open")
+
+# For the old os.walk method, please head to
+# https://python-forum.io/thread-8513.html
+
 class DirCtrl(wx.TreeCtrl):
     """
     A directory list made from wxTreeCtrl.
@@ -14,30 +29,14 @@ class DirCtrl(wx.TreeCtrl):
     * Copy-paste + right-click menu
     * Drag-n-drop
     * Hidden files detect (quite hard, may need to use C/C++)
+    * Directory only
     """
 
-    nodes: dict
+    def __init__(this, *args, **kw):
+        wx.TreeCtrl.__init__(this, *args, **kw)
+        this.AssignImageList(imgs)
 
-    def __init__(self, *args, **kw):
-        super().__init__(*args, **kw)
-
-        imgs = wx.ImageList(16, 16)
-        self.folderidx = imgs.Add(
-            wx.ArtProvider.GetBitmap(wx.ART_FOLDER, wx.ART_OTHER, (16, 16))
-        )
-        self.fileidx = imgs.Add(
-            wx.ArtProvider.GetBitmap(wx.ART_NORMAL_FILE, wx.ART_OTHER, (16, 16))
-        )
-
-        # Extras - opening folder icon
-        # Bind_ed with wxEVT_TREE_ITEM_EXPANDED event
-        self.openfolder = imgs.Add(
-            wx.ArtProvider.GetBitmap(wx.ART_FOLDER_OPEN, wx.ART_OTHER, (16, 16))
-        )
-
-        self.AssignImageList(imgs)
-
-    def SetFolder(self, path: str, newroot: bool = False):
+    def SetFolder(this, path: str, newroot: bool = False):
         """
         Make DirCtrl to open (a) directory.
         @param path (str): Target path
@@ -52,60 +51,119 @@ class DirCtrl(wx.TreeCtrl):
 
         def Expand(evt):
 
-            path = self.GetSelection()
-            fullpath = os.path.abspath(self.GetFullPath(path))
-            self.SetItemImage(path, self.openfolder, wx.TreeItemIcon_Expanded)
+            path = this.GetSelection()
+            fullpath = os.path.abspath(this.GetFullPath(path))
+            this.SetItemImage(path, this.openfolder, wx.TreeItemIcon_Expanded)
             
-            if len(os.listdir(fullpath)) == 0: self.SetItemHasChildren(path, False); return
+            if len(os.listdir(fullpath)) == 0: this.SetItemHasChildren(path, False); return
             # ^ blank folder? Get out eventually
 
             for item in os.listdir(fullpath):
                 craftedpath = CraftItems(fullpath, item)
-                icon = self.folderidx if os.path.isdir(craftedpath) else self.fileidx
+                icon = folderidx if os.path.isdir(craftedpath) else fileidx
 
-                self.nodes[craftedpath] = self.AppendItem(path, item, icon)
+                newitem = this.AppendItem(path, item, icon)
 
                 if os.path.isdir(craftedpath):
-                    self.SetItemHasChildren(self.nodes[craftedpath])
+                    this.SetItemHasChildren(newitem)
         
         path = os.path.normpath(path)
         if not os.path.isdir(path):
             raise NotADirectoryError(f"Directory not found or is a file: {path}")
 
-        if self.GetRootItem() and not newroot:
-            self.DeleteAllItems()
+        if this.GetRootItem() and not newroot:
+            this.DeleteAllItems()
 
-        self.nodes = {path: self.AddRoot(path, self.folderidx)}
-        self.SetItemHasChildren(self.nodes[path])
-        self.Bind(wx.EVT_TREE_ITEM_EXPANDED, Expand)
+        kickstart = this.AddRoot(path, this.folderidx)
+        this.SetItemHasChildren(kickstart)
+        this.Bind(wx.EVT_TREE_ITEM_EXPANDED, Expand)
 
-    def GetFullPath(self, item: wx.TreeItemId | None = None) -> str:
+    def GetFullPath(this, item: wx.TreeItemId | None = None) -> str:
         """
         Get the full path of an item.
+        The same work as wxGenericDirCtrl.GetPath.
         """
         if item == None:
-            parent = self.GetSelection()
+            parent = this.GetSelection()
         else:
             parent = item
 
         result = []
 
-        if parent == self.GetRootItem():
-            return self.GetItemText(parent)
+        if parent == this.GetRootItem():
+            return this.GetItemText(parent)
 
         def getroot():
             nonlocal parent
-            text = self.GetItemText(parent)
-            result.append(text)
-            if parent != self.GetRootItem():
-                parent = self.GetItemParent(parent)
+            text = this.GetItemText(parent)
+            result.insert(0, text)
+            if parent != this.GetRootItem():
+                parent = this.GetItemParent(parent)
                 getroot()
 
         getroot()
-        result.reverse()
+        # result.reverse()
 
         return CraftItems(*tuple(result))
 
 # @since 0.1.4: PatchedDirCtrl renamed to DirCtrl, and PatchedDirCtrl now points to that class
 # @brief "Patched".. seems not right:v (it's derived from wxTreeCtrl not wxGenericDirCtrl)
 PatchedDirCtrl = DirCtrl
+
+class DirList(wx.ListCtrl):
+    """
+    Unlike wxDirCtrl, wxDirList lists not only files + folders, but also their
+    last modified and item type, size. You will see it most in your file explorer,
+    the main pane.
+    This comes with check buttons, which is optional.
+    """
+
+    CurrPath: str
+
+    def __init__(this, *args, **kwds):
+        kwds["style"] = wx.LC_AUTOARRANGE \
+                    | wx.LC_EDIT_LABELS \
+                    | wx.LC_REPORT
+        wx.ListCtrl.__init__(this, *args, **kwds)
+        
+        this.InsertColumn(0, _("Name"))
+        this.InsertColumn(1, _("Item type"))
+        this.InsertColumn(2, _("Last modified"))
+        this.InsertColumn(3, _("Size"))
+
+        this.AssignImageList(imgs, wx.IMAGE_LIST_SMALL)
+
+        this.Bind(wx.EVT_LIST_ITEM_ACTIVATED, this.GoDir)
+    
+    def DrawItems(this, path: str = os.path.expanduser("~/")):
+        """
+        Fill the list control with items;)
+        """
+
+        this.DeleteAllItems()
+
+        if not os.path.isdir(path): raise NotADirectoryError(path)
+        this.CurrPath = path
+
+        for item in os.listdir(path):
+            if os.path.isdir(os.path.join(path, item)):
+                this.InsertItem(0, item, folderidx)
+                this.SetItem(0, 1, _("Folder"))
+            else:
+                this.InsertItem(0, item, fileidx)
+                this.SetItem(0, 1, _("File"))
+    
+    def GoDir(this, evt=None, path:str=""):
+        if evt and not path:
+            pos = evt.Index
+            name = this.GetItemText(pos)
+            item_type = this.GetItemText(pos, 1)
+
+            if item_type == _("Folder"):
+                this.DrawItems(os.path.join(this.CurrPath, name))
+        else:
+            if not path: raise Exception("Who the hell tell DirList.GoDir with no directory to go???")
+            this.DrawItems(path)
+    
+    def GoUp(this, evt):
+        this.DrawItems(os.path.dirname(this.CurrPath))
