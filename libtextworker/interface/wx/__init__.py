eed489489341c3de99eedac296170868b525e01f
lebao3105 <bao12345yocoo@gmail.com>
2023-10-25 23:26:32 +0700

    Fix hex-to-rgb converter code

--- libtextworker/interface/wx/__init__.py
+++ libtextworker/interface/wx/__init__.py
@@ -47,6 +47,7 @@ class ColorManager(ColorManager):
         # fore&back
 
         def hextorgb(value: str):
+            value = value.lstrip("#")
             lv = len(value)
             return tuple(int(value[i : i + lv // 3], 16) for i in range(0, lv, lv // 3))
 
