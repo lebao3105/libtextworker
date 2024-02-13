10f4addae78a92fc9e5f874ec2ef562d72a1fdb8
lebao3105 <bao12345yocoo@gmail.com>
2024-02-13 15:28:05 +0700

    Change library version

--- libtextworker/__init__.py
+++ libtextworker/__init__.py
@@ -8,19 +8,11 @@ import os.path
 
 from .general import GetCurrentDir, CraftItems, Importable, TOPLV_DIR
 
-__all__ = (
-    "__version__",
-    "EDITOR_DIR",
-    "Importable",
-    "LICENSES",
-    "LOG_PATH",
-    "THEMES_DIR",
-    "TOPLV_DIR",
-)
+__all__ = ( "__version__", "EDITOR_DIR", "Importable", "LOG_PATH", "THEMES_DIR", "TOPLV_DIR")
 
 # Setup translations
 LOCALEDIR = CraftItems(GetCurrentDir(__file__), "po")
-PROJDOMAIN = "libtextworker"  ## Project domain
+PROJDOMAIN = "libtextworker"
 
 if not os.path.exists(LOCALEDIR):
     LOCALEDIR = CraftItems(GetCurrentDir(__file__), "../po")
@@ -30,7 +22,7 @@ gettext.textdomain(PROJDOMAIN)
 _ = gettext.gettext
 
 # Something else;-;
-__version__ = "0.1.4"
+__version__ = "0.1.4b0"
 THEMES_DIR: str = ""
 EDITOR_DIR: str = ""
 LICENSES = rf'{os.path.normpath(CraftItems(GetCurrentDir(__file__), "licenses"))}'
